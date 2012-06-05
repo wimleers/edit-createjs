290d1b88a855cf0e813dd20d6991a48711bad132
Wim Leers <wim.leers@acquia.com>
2012-06-05 14:02:19 +0200

    Editable entities. Distinction between highlighting an editable and editing an editable. All behaviors work correctly.

--- js/ipe.js
+++ js/ipe.js
@@ -21,6 +21,8 @@ Drupal.ipe.init = function() {
   Drupal.ipe.state = {};
   // We always begin in view mode.
   Drupal.ipe.state.isViewing = true;
+  Drupal.ipe.state.entityBeingHighlighted = [];
+  Drupal.ipe.state.fieldBeingHighlighted = [];
   Drupal.ipe.state.fieldBeingEdited = [];
 
   // Transition between view/edit states.
@@ -36,14 +38,14 @@ Drupal.ipe.init = function() {
       var $f = Drupal.ipe.findEditableFields();
       Drupal.ipe.startEditableFields($f);
       var $e = Drupal.ipe.findEditableEntities();
-      // TODO: start editing entities
+      Drupal.ipe.startEditableEntities($e);
     }
     else if (!wasViewing && isViewing) {
       $('#ipe-overlay, .ipe-toolbar-container').remove();
       var $f = Drupal.ipe.findEditableFields();
       Drupal.ipe.stopEditableFields($f);
       var $e = Drupal.ipe.findEditableEntities();
-      // TODO: stop editing entities
+      Drupal.ipe.stopEditableEntities($e);
     }
     else {
       // No state change.
@@ -56,7 +58,8 @@ Drupal.ipe.renderToggle = function() {
 };
 
 Drupal.ipe.findEditableEntities = function() {
-  return $('.node');
+  var $content = $('#content');
+  return $('.node', $content);
 };
 
 Drupal.ipe.findEditableFields = function() {
@@ -65,10 +68,66 @@ Drupal.ipe.findEditableFields = function() {
   return $f;
 };
 
+Drupal.ipe.findEntityForField = function($f) {
+  return $f.parents('.node');
+};
+
+Drupal.ipe.startEditableEntities = function($e) {
+  $e
+  .addClass('ipe-editable-candidate ipe-editable entity')
+  .bind('mouseenter', function(e) {
+    var $e = $(this);
+    Drupal.ipe._ignoreToolbarMousing(e, function() {
+      console.log('entity:mouseenter');
+      Drupal.ipe.startHighlightEntity($e);
+    });
+  })
+  .bind('mouseleave', function(e) {
+    var $e = $(this);
+    Drupal.ipe._ignoreToolbarMousing(e, function() {
+      console.log('entity:mouseleave');
+      Drupal.ipe.stopHighlightEntity($e);
+    });
+  });
+};
+
+Drupal.ipe.stopEditableEntities = function($e) {
+  $e
+  .removeClass('ipe-editable-candidate ipe-editable entity highlighted')
+  .unbind('mouseenter')
+  .unbind('mouseleave');
+};
+
 Drupal.ipe.startEditableFields = function($f) {
   $f
-  .addClass('ipe-editable-candidate')
-  .bind('click', Drupal.ipe.startEditField)
+  .addClass('ipe-editable-candidate ipe-editable field')
+  .bind('mouseenter', function(e) {
+    var $f = $(this);
+    Drupal.ipe._ignoreToolbarMousing(e, function() {
+      console.log('field:mouseenter');
+      if (!$f.hasClass('editing')) {
+        Drupal.ipe.startHighlightField($f);
+      }
+      // Prevents the entity's mouse enter event from firing, in case their borders are one and the same.
+      e.stopPropagation();
+    });
+  })
+  .bind('mouseleave', function(e) {
+    var $f = $(this);
+    Drupal.ipe._ignoreToolbarMousing(e, function() {
+      console.log('field:mouseleave');
+      if (!$f.hasClass('editing')) {
+        Drupal.ipe.stopHighlightField($f);
+        // Leaving a field won't trigger the mouse enter event for the entity
+        // because the entity contains the field. Hence, do it manually.
+        var $e = Drupal.ipe.findEntityForField($f);
+        Drupal.ipe.startHighlightEntity($e);
+      }
+      // Prevent triggering the entity's mouse leave event.
+      e.stopPropagation();
+    });
+  })
+  .bind('click', function() { Drupal.ipe.startEditField($(this)); })
   // Some transformations are field-specific.
   .map(function() {
     // This does not get stripped when going back to view mode. The only way
@@ -78,48 +137,137 @@ Drupal.ipe.startEditableFields = function($f) {
   }); 
 };
 
-/**
- * Strips changes made by prepFields(), plus whatever editField() changed.
- */
 Drupal.ipe.stopEditableFields = function($f) {
   $f
-  .removeClass('ipe-editable-candidate editing')
-  .unbind('click')
+  .removeClass('ipe-editable-candidate ipe-editable field highlighted editing')
+  .unbind('mouseenter mouseleave click')
   .removeAttr('contenteditable');
-
-  $('.ipe-toolbar-container').remove();
 };
 
 Drupal.ipe.clickOverlay = function() {
+  console.log('clicked overlay');
   Drupal.ipe.stopEditField(Drupal.ipe.state.fieldBeingEdited);
 };
 
 Drupal.ipe.createToolbar = function($element) {
-  if ($element.prev('.ipe-toolbar-container').length > 0) {
-    return;
-  };
-  $('<div class="ipe-toolbar-container"><div class="ipe-toolbar"><div class="ipe-toolgroup ops"><a href="#" class="save gray-button">Save</a><a href="#" class="close gray-button"><span class="close"></span></a></div></div></div>').insertBefore($element);
-}
-
-Drupal.ipe.startEditField = function() {
-  if (Drupal.ipe.state.fieldBeingEdited.length > 0) {
-    Drupal.ipe.stopEditField(Drupal.ipe.state.fieldBeingEdited);
+  if (Drupal.ipe.getToolbar($element).length > 0) {
+    return false;
   }
-  $(this)
+  else {
+    $('<div class="ipe-toolbar-container"><div class="ipe-toolbar primary" /><div class="ipe-toolbar secondary" /></div>')
+    .insertBefore($element)
+    .bind('mouseenter', function(e) {
+      // Prevent triggering the entity's mouse enter event.
+      e.stopPropagation();
+    })
+    .bind('mouseleave', function(e) {
+      var rt = (e.relatedTarget) ? e.relatedTarget : e.toElement;
+      var el = $element[0];
+      if (rt != el && !jQuery.contains(el, e.relatedTarget)) {
+        console.log('triggering mouseleave on ', $element);
+        $element.trigger('mouseleave');
+      }
+      // Prevent triggering the entity's mouse leave event.
+      e.stopPropagation();
+    });
+    return true;
+  }
+};
+
+Drupal.ipe.getToolbar = function($element) {
+  return $element.prev('.ipe-toolbar-container');
+};
+
+Drupal.ipe.startHighlightEntity = function($e) {
+  console.log('startHighlightEntity');
+  if (Drupal.ipe.createToolbar($e)) {
+    Drupal.ipe.getToolbar($e)
+    .find('.ipe-toolbar.primary:not(:has(.ipe-toolgroup.entity))')
+    .append('<div class="ipe-toolgroup entity"><a href="#" class="blue-button">Edit full node</a></div>');
+  }
+  $e.addClass('highlighted');
+
+  Drupal.ipe.state.entityBeingHighlighted = $e;
+};
+
+Drupal.ipe.stopHighlightEntity = function($e) {
+  console.log('stopHighlightEntity');
+  $e.removeClass('highlighted');
+
+  Drupal.ipe.getToolbar($e).remove();
+
+  Drupal.ipe.state.entityBeingHiglighted = [];
+};
+
+Drupal.ipe.startHighlightField = function($f) {
+  console.log('startHighlightField');
+  if (Drupal.ipe.state.entityBeingHighlighted.length > 0) {
+    var $e = Drupal.ipe.findEntityForField($f);
+    Drupal.ipe.stopHighlightEntity($e);
+  }
+  if (Drupal.ipe.createToolbar($f)) {
+    Drupal.ipe.getToolbar($f)
+    .find('.ipe-toolbar.primary:not(:has(.ipe-toolgroup.info))')
+    .append('<div class="ipe-toolgroup info"><a href="#" class="blank-button">Field name</a></div>');
+  }
+  $f.addClass('highlighted');
+
+  Drupal.ipe.state.fieldBeingHighlighted = $f;
+};
+
+Drupal.ipe.stopHighlightField = function($f) {
+  console.log('stopHighlightField');
+  if ($f.length == 0) {
+    return;
+  }
+  else if (Drupal.ipe.state.fieldBeingEdited.length > 0 && $f[0] == Drupal.ipe.state.fieldBeingEdited[0]) {
+    return;
+  }
+
+  $f.removeClass('highlighted');
+
+  Drupal.ipe.getToolbar($f).remove()
+};
+
+Drupal.ipe.startEditField = function($f) {
+  if (Drupal.ipe.state.fieldBeingEdited.length > 0 && Drupal.ipe.state.fieldBeingEdited[0] == $f[0]) {
+    return;
+  }
+
+  console.log('startEditField: ', $f);
+  if (Drupal.ipe.state.fieldBeingHighlighted[0] != $f[0]) {
+    Drupal.ipe.startHighlightField($f);
+  }
+  $f
   .addClass('editing')
   .attr('contenteditable', true);
 
-  Drupal.ipe.createToolbar($(this));
+  // While editing, don't show *any* other field or entity as editable.
+  $('.ipe-editable-candidate').not('.editing').removeClass('ipe-editable');
 
-  Drupal.ipe.state.fieldBeingEdited = $(this);
+  Drupal.ipe.getToolbar($f)
+  .find('.ipe-toolbar.secondary:not(:has(.ipe-toolgroup.ops))')
+  .append('<div class="ipe-toolgroup ops"><a href="#" class="save gray-button">Save</a><a href="#" class="close gray-button"><span class="close"></span></a></div>');
+
+  Drupal.ipe.state.fieldBeingEdited = $f;
 };
 
 Drupal.ipe.stopEditField = function($f) {
+  console.log('stopEditField: ', $f);
+  if ($f.length == 0) {
+    return;
+  }
+
   $f
-  .removeClass('editing')
-  .removeAttr('contenteditable')
-  // Remove IPE toolbar.
-  .prev('.ipe-toolbar-container').remove();
+  .removeClass('highlighted editing')
+  .removeAttr('contenteditable');
+
+  // Make the other fields and entities editable again.
+  $('.ipe-editable-candidate').addClass('ipe-editable');
+
+  Drupal.ipe.getToolbar($f).remove()
+
+  Drupal.ipe.state.fieldBeingEdited = [];
 };
 
 Drupal.ipe._getBgColor = function($e) {
@@ -137,7 +285,28 @@ Drupal.ipe._getBgColor = function($e) {
     return Drupal.ipe._getBgColor($e.parent());
   }
   return c;
-}
+};
+
+Drupal.ipe._ignoreToolbarMousing = function(e, callback) {
+  var rt;
+  if (e.type == 'mouseleave') {
+    rt = (e.relatedTarget) ? e.relatedTarget : e.toElement;
+  }
+  else if (e.type == 'mouseenter') {
+    rt = (e.relatedTarget) ? e.relatedTarget : e.fromElement;
+  }
+  else {
+    return;
+  }
+
+  if ($(rt).parents(".ipe-toolbar-container").length > 0) {
+    e.stopPropagation();
+    return;
+  }
+  else {
+    callback();
+  }
+};
 
 })(jQuery);
 
