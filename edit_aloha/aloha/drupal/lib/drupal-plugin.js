8c78eac6a09f8a45770c8e312caa88b973a769f5
Wim Leers <wim.leers@acquia.com>
2012-07-26 15:36:14 +0200

    Drupal AE plug-in: 2 spaces for indentation everywhere.

--- edit_aloha/aloha/drupal/lib/drupal-plugin.js
+++ edit_aloha/aloha/drupal/lib/drupal-plugin.js
@@ -14,97 +14,97 @@ define( [
     DrupalContentHandler,
     console
 ) {
-    'use strict';
+  'use strict';
 
-    var pluginNamespace = 'aloha-drupal';
+  var pluginNamespace = 'aloha-drupal';
 
-    return Plugin.create('drupal', {
-        /**
-         * Configure the available languages.
-        */
-        languages: [ 'en' ],
+  return Plugin.create('drupal', {
+    /**
+     * Configure the available languages.
+    */
+    languages: [ 'en' ],
 
-        /**
-         * Default configuration.
-        */
-        config: [ ],
+    /**
+     * Default configuration.
+    */
+    config: [ ],
 
-        /**
-         * Initialize the plug-in.
-        */
-        init: function () {
-            var elementMapping,
-                allowedTags;
+    /**
+     * Initialize the plug-in.
+    */
+    init: function () {
+      var elementMapping,
+          allowedTags;
 
-            elementMapping = {
-                'a': 'aloha-icon-link',
-                'b': 'aloha-icon-bold',
-                'strong': 'aloha-icon-strong',
-                'i': 'aloha-icon-italic',
-                'em': 'aloha-icon-emphasis',
-                'del': 'aloha-icon-strikethrough',
-                'sub': 'aloha-icon-subscript',
-                'sup': 'aloha-icon-superscript',
-                'u': 'aloha-icon-underline',
-                's': 'aloha-icon-strikethrough2',
-                'ol': 'aloha-icon-orderedlist',
-                'ul': 'aloha-icon-unorderedlist',
-                'p': 'aloha-large-icon-p',
-                'pre': 'aloha-large-icon-pre',
-                'h1': 'aloha-large-icon-h1',
-                'h2': 'aloha-large-icon-h2',
-                'h3': 'aloha-large-icon-h3',
-                'h4': 'aloha-large-icon-h4',
-                'h5': 'aloha-large-icon-h5',
-                'h6': 'aloha-large-icon-h6',
-            };
+      elementMapping = {
+          'a': 'aloha-icon-link',
+          'b': 'aloha-icon-bold',
+          'strong': 'aloha-icon-strong',
+          'i': 'aloha-icon-italic',
+          'em': 'aloha-icon-emphasis',
+          'del': 'aloha-icon-strikethrough',
+          'sub': 'aloha-icon-subscript',
+          'sup': 'aloha-icon-superscript',
+          'u': 'aloha-icon-underline',
+          's': 'aloha-icon-strikethrough2',
+          'ol': 'aloha-icon-orderedlist',
+          'ul': 'aloha-icon-unorderedlist',
+          'p': 'aloha-large-icon-p',
+          'pre': 'aloha-large-icon-pre',
+          'h1': 'aloha-large-icon-h1',
+          'h2': 'aloha-large-icon-h2',
+          'h3': 'aloha-large-icon-h3',
+          'h4': 'aloha-large-icon-h4',
+          'h5': 'aloha-large-icon-h5',
+          'h6': 'aloha-large-icon-h6',
+      };
 
-            // register DrupalContentHandler
-            // @todo add data-edit-allowed-tags attribute also to the backend or adapt the DrupalContentHandler
-            ContentHandlerManager.register('drupal', DrupalContentHandler);
+      // register DrupalContentHandler
+      // @todo add data-edit-allowed-tags attribute also to the backend or adapt the DrupalContentHandler
+      ContentHandlerManager.register('drupal', DrupalContentHandler);
 
-            Aloha.bind( 'aloha-editable-activated', function( $event, params ) {
-                var allowedTagsList = Aloha.activeEditable.obj
-                                      .closest('.edit-field')
-                                      .data('edit-allowed-tags');
+      Aloha.bind( 'aloha-editable-activated', function( $event, params ) {
+        var allowedTagsList = Aloha.activeEditable.obj
+                              .closest('.edit-field')
+                              .data('edit-allowed-tags');
 
-                if (allowedTagsList) {
-                  allowedTags = allowedTagsList.split(',');
+        if (allowedTagsList) {
+          allowedTags = allowedTagsList.split(',');
 
-                  jQuery.each(elementMapping, function(element, className) {
-                    if (jQuery.inArray(element, allowedTags) == -1) {
-                      jQuery('.' + className).closest('button').hide();
-                    }
-                  });
-                }
-                
-                // Clean up empty component groups.
-                jQuery.each(jQuery('.aloha-ui-component-group'), function(){
-                    // Check for hidden nodes and empty nodes.
-                    var cc = 0;
-                    jQuery.each(jQuery(this).children(), function() {
-                        if (jQuery(this).css('display') == 'none' || !jQuery(this).text()) {
-                            cc++;
-                        }
-                    });
-                    if (cc == jQuery(this).children().length) {
-                        jQuery(this).hide();
-                    }
-                });
-            });
-
-            Aloha.bind( 'aloha-editable-deactivated', function( $event, params ) {
-                jQuery.each(elementMapping, function(element, className) {
-                    if (jQuery.inArray(element, allowedTags) == -1) {
-                      jQuery('.' + className).closest('button').show();
-                    }
-                });
-
-                // Restore previously empty component groups.
-                jQuery.each(jQuery('.aloha-ui-component-group'), function() {
-                    jQuery(this).show();
-                });
-            });
+          jQuery.each(elementMapping, function(element, className) {
+            if (jQuery.inArray(element, allowedTags) == -1) {
+              jQuery('.' + className).closest('button').hide();
+            }
+          });
         }
-    });
+
+        // Clean up empty component groups.
+        jQuery.each(jQuery('.aloha-ui-component-group'), function(){
+          // Check for hidden nodes and empty nodes.
+          var cc = 0;
+          jQuery.each(jQuery(this).children(), function() {
+            if (jQuery(this).css('display') == 'none' || !jQuery(this).text()) {
+              cc++;
+            }
+          });
+          if (cc == jQuery(this).children().length) {
+            jQuery(this).hide();
+          }
+        });
+      });
+
+      Aloha.bind( 'aloha-editable-deactivated', function( $event, params ) {
+        jQuery.each(elementMapping, function(element, className) {
+          if (jQuery.inArray(element, allowedTags) == -1) {
+            jQuery('.' + className).closest('button').show();
+          }
+        });
+
+        // Restore previously empty component groups.
+        jQuery.each(jQuery('.aloha-ui-component-group'), function() {
+            jQuery(this).show();
+        });
+      });
+    }
+  });
 });
