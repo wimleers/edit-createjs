2ef3de42f598c1ae070014f1cd0a08d63e827d91
Wim Leers <wim.leers@acquia.com>
2012-07-26 18:44:08 +0200

    Make all images captionable by AE's Captioned Image plug-in.

--- edit_aloha/js/edit_aloha.js
+++ edit_aloha/js/edit_aloha.js
@@ -24,6 +24,8 @@ Drupal.edit.wysiwyg.edit_aloha = {
       $editable.attr('id', id);
     }
 
+    this._markImages($editable);
+
     Aloha.jQuery('#' + id).aloha();
   },
 
@@ -42,6 +44,23 @@ Drupal.edit.wysiwyg.edit_aloha = {
     if (id.match(/^edit-aloha-\d+$/) != null) {
       $editable.removeAttr('id');
     }
+
+    this._unmarkImages($editable);
+  },
+
+  // The AE Captioned Image plug-in only works on images that have a certain
+  // class, mark images that should be captionable with this class.
+  _markImages: function($editable) {
+    // @TODO: For now, make *all* images captionable.
+    $editable
+    .find('img')
+    .addClass('aloha-captioned-image');
+  },
+
+  _unmarkImages: function($editable) {
+    $editable
+    .find('.aloha-captioned-image')
+    .removeClass('aloha-captioned-image');
   }
 };
 
