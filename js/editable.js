170677d7c454c24e28244c021c1e1b103c47b744
Henri Bergius <henri.bergius@iki.fi>
2012-08-23 17:13:01 +0200

    Initial Create.js editor widget for Drupal forms

--- js/editable.js
+++ js/editable.js
@@ -5,20 +5,36 @@
   // for the fact that Spark is using custom data attributes and not RDFa
   // to communicate editable fields.
   jQuery.widget('Drupal.createEditable', jQuery.Midgard.midgardEditable, {
+    _create: function () {
+      this.vie = this.options.vie;
+
+      this.options.editors.direct = {
+        widget: 'alohaWidget',
+        options: {}
+      }
+      this.options.editors.form = {
+        widget: 'drupalFormWidget',
+        options: {}
+      }
+    },
+
     findEditableElements: function (callback) {
-      var fields = Drupal.edit.findEditableFields(this.element);
       var model = this.options.model;
-      fields.filter(function () {
+      var fields = Drupal.edit.util.findEditableFields(this.element).filter(function () {
         return Drupal.edit.util.getElementSubject(jQuery(this)) == model.getSubjectUri();
-      }).each(callback);
+      });
+      Drupal.edit.findEditablesForFields(fields).each(callback);
     },
 
     getElementPredicate: function (element) {
        return Drupal.edit.util.getElementPredicate(jQuery(element));
     },
 
-    _editorWidget: function () {
-      return 'alohaWidget';
+    _editorName: function (data) {
+      if (Drupal.settings.edit.wysiwyg && jQuery(data.element).hasClass('edit-type-direct')) {
+        return 'direct';
+      }
+      return 'form';
     }
   });
 })(jQuery);
---
+++ js/formwidget.js
@@ -0,0 +1,52 @@
+(function (jQuery, undefined) {
+  // # Drupal form-based editing widget for Create.js
+  jQuery.widget('Drupal.drupalFormWidget', jQuery.Create.editWidget, {
+    options: {
+      editorOptions: {},
+      disabled: true
+    },
+
+    enable: function () {
+      this.options.disabled = false;
+
+      var self = this;
+      var formLoader = function () {
+        self.loadForm();
+        self.element.unbind('click', formLoader);
+      };
+
+      this.element.bind('click', formLoader);
+    },
+
+    loadForm: function () {
+      if (Drupal.edit.form.create(this.element)) {
+        this.element
+        .addClass('edit-belowoverlay')
+        .removeClass('edit-highlighted edit-editable');
+
+        Drupal.edit.form.get(this.element)
+        .find('.edit-form')
+        .addClass('edit-editable edit-highlighted edit-editing')
+        .css('background-color', this.element.data('edit-background-color'));
+      }
+
+      var field = Drupal.edit.findFieldForEditable(this.element);
+      Drupal.edit.editables._loadForm(this.element, field);
+    },
+
+    disable: function () {
+      this.options.disabled = true;
+    },
+
+    _initialize: function () {
+      var self = this;
+      jQuery(this.element).bind('focus', function (event) {
+        self.options.activated(); 
+      });
+      
+      jQuery(this.element).bind('blur', function (event) {
+        self.options.deactivated(); 
+      });
+    }
+  });
+})(jQuery);
