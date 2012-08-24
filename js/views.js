6361ae45d2f3e5c47df68febbf1a39358ce6316e
Henri Bergius <henri.bergius@iki.fi>
2012-08-24 11:52:19 +0200

    Document EditableView

--- js/views.js
+++ js/views.js
@@ -2,8 +2,16 @@
   Drupal.edit = Drupal.edit || {};
   Drupal.edit.views = Drupal.edit.views || {};
 
+  // ## EditableView
+  //
+  // This view wraps a editable DOM element, and connects it
+  // with the VIE Entity instance. Whenever the particular
+  // attribute (predicate) of the instance changes, whether
+  // due to user interaction or some AJAX call, the contents
+  // of the DOM element will be automatically updated.
   Drupal.edit.views.EditableView = Backbone.View.extend({
     predicate: null,
+
     initialize: function (options) {
       this.predicate = '<http://viejs.org/ns/' + options.predicate + '>';
       _.bindAll(this, 'render');
@@ -14,4 +22,5 @@
       jQuery(this.el).html(this.model.get(this.predicate)); 
     }
   });
+
 })(jQuery);
